7b7a69852b889d99fd4e1682a88fde69d0ed3f48
Shengjiu Wang <shengjiu.wang@nxp.com>
2024-02-23 11:37:47 +0800

    LF-11582-2: update copyright for voicespot and voiceseeker
    update copyright for voicespot and voiceseeker files
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- utils/rdsp_common_utils/include/RdspSysdefsParser.h
+++ utils/rdsp_common_utils/include/RdspSysdefsParser.h
@@ -1,10 +1,14 @@
 /*
- * Copyright (c) 2021 by Retune DSP. This code is the confidential and
- * proprietary property of Retune DSP and the possession or use of this
- * file requires a written license from Retune DSP.
+ * Copyright (c) 2021 by Retune DSP.
+ * Copyright 2022-2024 NXP
  *
- * Contact information: info@retune-dsp.com
- *                      www.retune-dsp.com
+ * NXP Confidential. This software is owned or controlled by NXP
+ * and may only be used strictly in accordance with the applicable license terms.
+ * By expressly accepting such terms or by downloading, installing,
+ * activating and/or otherwise using the software, you are agreeing that you have read,
+ * and that you agree to comply with and are bound by, such license terms.
+ * If you do not agree to be bound by the applicable license terms,
+ * then you may not retain, install, activate or otherwise use the software.
  */
 
 
--- voiceseeker/platforms/iMX8M_CortexA53/include/libVoiceSeekerLight.h
+++ voiceseeker/platforms/iMX8M_CortexA53/include/libVoiceSeekerLight.h
@@ -1,9 +1,15 @@
 /*
-Copyright 2021 by Retune DSP
-Copyright 2022 NXP
-
-NXP Confidential. This software is owned or controlled by NXP and may only be used strictly in accordance with the applicable license terms.  By expressly accepting such terms or by downloading, installing, activating and/or otherwise using the software, you are agreeing that you have read, and that you agree to comply with and are bound by, such license terms.  If you do not agree to be bound by the applicable license terms, then you may not retain, install, activate or otherwise use the software.
-*/
+ * Copyright (c) 2021 by Retune DSP.
+ * Copyright 2022-2024 NXP
+ *
+ * NXP Confidential. This software is owned or controlled by NXP
+ * and may only be used strictly in accordance with the applicable license terms.
+ * By expressly accepting such terms or by downloading, installing,
+ * activating and/or otherwise using the software, you are agreeing that you have read,
+ * and that you agree to comply with and are bound by, such license terms.
+ * If you do not agree to be bound by the applicable license terms,
+ * then you may not retain, install, activate or otherwise use the software.
+ */
 
 #ifndef RDSP_VOICESEEKER_LIGHT_PLUGIN_H_
 #define RDSP_VOICESEEKER_LIGHT_PLUGIN_H_
